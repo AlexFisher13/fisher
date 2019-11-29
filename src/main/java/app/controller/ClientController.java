2a69280512885d478c081227db84aa8c15e53ebe
Tkachenko <alexfisher13@yandex.ru>
2019-11-29 12:36:13 +0300

    split validationExceptionHandler. Add new error pages

--- src/main/java/app/controller/ClientController.java
+++ src/main/java/app/controller/ClientController.java
@@ -4,13 +4,17 @@ import app.domain.Client;
 import app.repos.ClientRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.validation.Valid;
+import javax.validation.constraints.Size;
 import java.util.Map;
 
 @Controller
+@Validated
 public class ClientController {
 
     @Autowired
@@ -24,11 +28,10 @@ public class ClientController {
     }
 
     @PostMapping("/addclient")
-    public String addClient(
-            @RequestParam String firstname,
-            @RequestParam String lastname,
-            @RequestParam String midname,
-            Map<String, Object> model) {
+    public String addClient(@Valid @Size(min = 3, max = 15) @RequestParam String firstname,
+                            @RequestParam String lastname,
+                            @RequestParam String midname,
+                            Map<String, Object> model) {
         Client newClient = new Client(firstname, lastname, midname);
         clientRepo.save(newClient);
         Iterable<Client> clients = clientRepo.findAll();
--- src/main/java/app/controller/ControllerExceptionHandler.java
+++ src/main/java/app/controller/ControllerExceptionHandler.java
@@ -9,11 +9,19 @@ import javax.validation.ConstraintViolationException;
 @ControllerAdvice
 class ControllerExceptionHandler {
     @ExceptionHandler(ConstraintViolationException.class)
-    public String handleValidation() {
-        return "validation-error";
+    public String handleValidation(ConstraintViolationException e) {
+
+        switch (e.getMessage()) {
+            case "addCard.number: должно соответствовать шаблону \"\\d{16}\"":
+                return "error-card";
+            case "addClient.firstname: размер должен быть между 3 и 15":
+                return "error-client";
+        }
+        return "error-default";
     }
+
     @ExceptionHandler(DuplicateMemberException.class)
     public String handleDuplicat() {
-        return "duplicate-error";
+        return "error-duplicate";
     }
 }
