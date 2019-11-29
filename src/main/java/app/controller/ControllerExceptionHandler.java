45e7e57caa58899a87d19ffc186fb5e468872464
Tkachenko <alexfisher13@yandex.ru>
2019-11-29 14:40:24 +0300

    refactor exception handler

--- src/main/java/app/controller/ControllerExceptionHandler.java
+++ src/main/java/app/controller/ControllerExceptionHandler.java
@@ -1,27 +1,18 @@
 package app.controller;
 
-import javassist.bytecode.DuplicateMemberException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-
-import javax.validation.ConstraintViolationException;
+import org.springframework.web.servlet.ModelAndView;
 
 @ControllerAdvice
 class ControllerExceptionHandler {
-    @ExceptionHandler(ConstraintViolationException.class)
-    public String handleValidation(ConstraintViolationException e) {
+    @ExceptionHandler(Exception.class)
+    public ModelAndView handleValidation(Exception e) {
 
-        switch (e.getMessage()) {
-            case "addCard.number: должно соответствовать шаблону \"\\d{16}\"":
-                return "error-card";
-            case "addClient.firstname: размер должен быть между 3 и 15":
-                return "error-client";
-        }
-        return "error-default";
+        ModelAndView mav = new ModelAndView();
+        mav.addObject("message", e.getMessage());
+        mav.setViewName("error");
+        return mav;
     }
 
-    @ExceptionHandler(DuplicateMemberException.class)
-    public String handleDuplicat() {
-        return "error-duplicate";
-    }
 }
